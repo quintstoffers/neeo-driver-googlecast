ab3a12ec3992d0860d004f2e87dc55187fad5580
Quint Stoffers <quint@swydo.com>
2018-05-25 22:27:28 +0200

    BREAKING CHANGE: migrate to using CLI provided by neeo-sdk

---
+++ devices/index.js
@@ -0,0 +1,5 @@
+const { googlecastDevice } = require('../lib/device');
+
+module.exports = {
+    devices: [googlecastDevice]
+};
--- index.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-require('reify');
-require('./lib/index');
